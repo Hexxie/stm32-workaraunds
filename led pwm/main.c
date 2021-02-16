f44a17b61be370435323f6bc5f6d7c8a43e7c95b
hexxie <arusakova917@gmail.com>
2021-02-16 19:55:45 +0200

    Added button

--- led pwm/main.c
+++ led pwm/main.c
@@ -27,6 +27,11 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef enum
+{
+  RELEASED = 0u,
+  PRESSED
+} ButtonState;
 
 /* USER CODE END PTD */
 
@@ -91,29 +96,41 @@ int main(void)
   /* USER CODE BEGIN 2 */
 	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 	uint16_t i = 0, d = 0;
+	ButtonState state = RELEASED;
+	/* USER CODE END 2 */
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
-		for (i = 0; i < 6000; i++) {
-			TIM1->CCR1 = i;
-			for (d = 0; d < 100; d++) {
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_RESET)
+		{
+			if(state == RELEASED) {
+				for (i = 0; i < 6000; i++) {
+					TIM1->CCR1 = i;
+					for (d = 0; d < 100; d++) {}
+				}
+				state = PRESSED;
 			}
-		}
-		for (i = 6000; i > 0; i--) {
-			TIM1->CCR1 = i;
-			for (d = 0; d < 100; d++) {
+
+		} else {
+			if(state == PRESSED) {
+				for (i = 6000; i > 0; i--) {
+					TIM1->CCR1 = i;
+					for (d = 0; d < 100; d++) {
+					}
+				}
+				state = RELEASED;
 			}
+
+
 		}
 
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
@@ -172,7 +189,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 1;
+  htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 6000;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -237,16 +254,23 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
 
-  /*Configure GPIO pin : PA3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
--- led pwm/readme.md
+++ led pwm/readme.md
@@ -1,3 +1,6 @@
 This is a pwm setup for stm32f103c8
 
 Connect the led to pin PA8 and see the magic
+
+Upd: second commit - led with button
+Connect button to PA0 and see pwm magic, pressing on it
